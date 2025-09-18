d6d4a563787500b90a1c6e917a6f1bb79b9fe50e
IssacSegura <issacsegura2005@gmail.com>
2025-09-17 20:38:46 -0600

    commit semana 2

---
+++ Tienda/src/main/java/Tienda/demo/TiendaIssacApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIssacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIssacApplication.class, args);
+	}
+
+}
