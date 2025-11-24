a9bcace218c6c16a38094f2d36d786f267570c7a
IssacSegura <issacsegura2005@gmail.com>
2025-11-23 23:24:12 -0600

    semana 11

--- Tienda/src/main/java/Tienda/demo/repository/RolRepository.java
+++ Tienda/src/main/java/Tienda/demo/repository/RolRepository.java
@@ -4,10 +4,12 @@
  */
 package Tienda.demo.repository;
 
-/**
- *
- * @author issac
- */
-public interface RolRepository {
-    
+import Tienda.demo.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+
 }
